1b79cc6be8b974bf4e233420615ae48591b1c5e1
m.habibian <mojtabaa.hn@gmail.com>
2023-04-17 13:28:43 +0330

    feat: transition private attributes to protected

--- src/backbone_redis_cache/redis_cache.py
+++ src/backbone_redis_cache/redis_cache.py
@@ -11,20 +11,20 @@ class RedisCache:
             serializer : Optional[Callable] = json.dumps,
             deserializer : Optional[Callable] = json.loads,
     ) -> None:
-        self.__connection = connection
-        self.__prefix = prefix
-        self.__deserialize = deserializer
-        self.__serialize = serializer
+        self._connection = connection
+        self._prefix = prefix
+        self._deserialize = deserializer
+        self._serialize = serializer
 
     async def get(self, key: str, default=None) -> Any:
-        result: str = await self.__connection.get(self.__prefix + key)
-        return self.__deserialize(result) if result is not None else default
+        result: str = await self._connection.get(self._prefix + key)
+        return self._deserialize(result) if result is not None else default
 
     async def exists(self, key: str) -> bool:
-        return await self.__connection.exists(self.__prefix + key) != 0
+        return await self._connection.exists(self._prefix + key) != 0
 
     async def set(self, key: str, value: Any, seconds: Optional[int] = None) -> None:
-        await self.__connection.set(self.__prefix + key, self.__serialize(value), ex=seconds)
+        await self._connection.set(self._prefix + key, self._serialize(value), ex=seconds)
 
     async def cset(self, key: str, increment: int = 1, seconds: Optional[int] = None) -> None:
         number = await self.__connection.incrby(self.__prefix + key, increment)
@@ -32,15 +32,15 @@ class RedisCache:
             await self.__connection.pexpire(self.__prefix + key, seconds * 1000)
 
     async def mset(self, dictionary: Dict[str, Any], seconds: Optional[int] = None) -> None:
-        pipe = self.__connection.pipeline()
+        pipe = self._connection.pipeline()
         for key, value in dictionary.items():
-            await pipe.set(self.__prefix + key, self.__serialize(value), ex=seconds)
+            await pipe.set(self._prefix + key, self._serialize(value), ex=seconds)
         await pipe.execute()
 
     async def mget(self, keys: List[str], default=None) -> List[Any]:
-        results = await self.__connection.mget([self.__prefix + key for key in keys])
+        results = await self._connection.mget([self._prefix + key for key in keys])
         return [
-            self.__deserialize(result) if result is not None else default
+            self._deserialize(result) if result is not None else default
             for result in results
         ]
 
@@ -49,8 +49,8 @@ class RedisCache:
         return 0 if number is None else int(number)
 
     async def forget(self, key) -> None:
-        await self.__connection.delete(self.__prefix + key)
+        await self._connection.delete(self._prefix + key)
 
     async def flush(self):
-        await self.__connection.flushdb()
-        await self.__connection.flushall()
+        await self._connection.flushdb()
+        await self._connection.flushall()
